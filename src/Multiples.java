9c26d61d138a31e788b0f598854cb70dcceaa193
DavidFJ207 <david-f-jimenez@live.com>
2023-09-15 12:55:05 -0400

    Lab 1

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,29 @@
 public class Multiples {
+    public static void main(String[] args) {
+        int i = 1;
+        int total = 0;
+        while (i < 1000) {
+
+            boolean multipleof3 = i % 3 == 0;
+            boolean multipleof5 = i % 5 == 0;
+
+
+            // Print our appropriate result.
+            if (multipleof3 && multipleof5) {
+                i = i + 1;
+                total = total + 1;
+            } else if (multipleof3) {
+                i = i + 1;
+                total = total + 1;
+            } else if (multipleof5) {
+                i = i + 1;
+                total = total + 1;
+            } else {
+                i = i + 1;
+
+            }
+
+        }
+        System.out.println(total);
+    }
 }
--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,20 @@
 public class Reduce {
+    public static void main(String[] args) {
+        Integer n = 100;
+        Integer total = 0;
+
+        while (n > 0) {
+
+
+            if (n % 2 != 0) {
+                n = n/2 ;
+                total = total + 1;
+            } else {
+                n = n - 1;
+                total = total + 1;
+            }
+
+        }
+        System.out.println(total);
+    }
 }
